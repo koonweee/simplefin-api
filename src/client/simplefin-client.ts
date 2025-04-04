8b4c416b02936c06b48d0d95fe1e3561b87f02bc
Jeremy Tan <jeremy.tan@newfront.com>
2025-04-03 23:58:43 -0700

    Types and API client cleanup

--- src/client/simplefin-client.ts
+++ src/client/simplefin-client.ts
@@ -1,82 +1,61 @@
 import axios, { AxiosInstance } from 'axios';
 import {
-  SimpleFINConfig,
-  ServerInfo,
   AccountSet,
-  AccountsQueryParams
+  AccountsQueryParams,
+  SimpleFINClientConfig
 } from '../types';
 
 export class SimpleFINClient {
   private client: AxiosInstance;
-  private config: SimpleFINConfig;
+  private config: SimpleFINClientConfig;
 
-  constructor(config: SimpleFINConfig) {
+  /**
+   * Base64 decodes a SimpleFIN token to get a claim URL
+   * Then makes a POST request to the claim URL to get an access URL
+   * Note that each token can only be claimed once
+   * (the claim URL expires after the first POST request)
+   * @param simplefinToken Base64 encoded token received from user
+   * @returns Access URL
+   */
+  static async claimAccessURL(simplefinToken: string): Promise<string> {
+    const decodedUrl = Buffer.from(simplefinToken, 'base64').toString('utf-8');
+    const response = await axios.post(decodedUrl);
+    return response.data;
+  }
+
+  /**
+   * Create a new SimpleFIN client from an access URL
+   * @param config Configuration for the client (access URL)
+   * @returns instance of SimpleFIN client
+   */
+  constructor(config: SimpleFINClientConfig) {
     this.config = config;
     this.client = axios.create({
-      baseURL: config.baseUrl,
-      headers: config.accessToken ? {
-        Authorization: `Basic ${config.accessToken}`
-      } : {}
+      baseURL: config.accessUrl,
     });
   }
 
   /**
-   * Get server information and supported versions
-   */
-  async getInfo(): Promise<ServerInfo> {
-    const response = await this.client.get('/info');
-    return response.data;
-  }
-
-  /**
-   * Get the URL for creating a new SimpleFIN token
-   */
-  getCreateUrl(): string {
-    return `${this.config.baseUrl}/create`;
-  }
-
-  /**
-   * Claim a SimpleFIN token to get an access URL
-   * @param token Base64 encoded token received from user
-   */
-  async claimToken(token: string): Promise<string> {
-    const decodedUrl = Buffer.from(token, 'base64').toString('utf-8');
-    const response = await this.client.post(decodedUrl);
-    return response.data;
-  }
-
-  /**
-   * Get accounts and transactions data
+   * Retrieve one or many accounts (account information and transactions)
+   * Makes a GET request to the `/accounts` endpoint on the access URL
    * @param params Query parameters for filtering accounts and transactions
    */
   async getAccounts(params?: AccountsQueryParams): Promise<AccountSet> {
     const queryParams = new URLSearchParams();
 
     if (params) {
-      if (params.start_date) queryParams.append('start-date', params.start_date.toString());
-      if (params.end_date) queryParams.append('end-date', params.end_date.toString());
-      if (params.pending) queryParams.append('pending', '1');
-      if (params.balances_only) queryParams.append('balances-only', '1');
-      if (params.account) {
-        params.account.forEach(acc => queryParams.append('account', acc));
+      if (params.startDate) queryParams.append('start-date', params.startDate.toString());
+      if (params.endDate) queryParams.append('end-date', params.endDate.toString());
+      if (params.includePending) queryParams.append('pending', '1');
+      if (params.onlyReturnBalances) queryParams.append('balances-only', '1');
+      if (params.accountIds) {
+        params.accountIds.forEach(acc => queryParams.append('account', acc));
       }
     }
 
-    const response = await this.client.get(`/accounts${queryParams.toString() ? `?${queryParams.toString()}` : ''}`);
-    return response.data;
-  }
+    const queryString = queryParams.toString().length > 0 ? `?${queryParams.toString()}` : '';
 
-  /**
-   * Update the access token for authenticated requests
-   * @param accessToken Base64 encoded access token
-   */
-  setAccessToken(accessToken: string) {
-    this.config.accessToken = accessToken;
-    this.client = axios.create({
-      baseURL: this.config.baseUrl,
-      headers: {
-        Authorization: `Basic ${accessToken}`
-      }
-    });
+    const response = await this.client.get(`/accounts${queryString}`);
+    return response.data;
   }
 }
